380203dcf09264130c9914f1f38f36520672a5d4
Bruno Sousa <bsousa86@Bsousa86.local>
2018-10-17 14:14:12 +0100

    minor fix to mongo db connect

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const postsRoute = require('./routes/postsRoute');
 const comRoute   = require('./routes/commentsRoute');
 const app = express();
 
-mongoose.connect('mongodb://bsousa86:' + process.env.MONGO_DB_PW + '@webappproject-shard-00-00-b1buu.mongodb.net:27017,webappproject-shard-00-01-b1buu.mongodb.net:27017,webappproject-shard-00-02-b1buu.mongodb.net:27017/test?ssl=true&replicaSet=WebAppProject-shard-0&authSource=admin&retryWrites=true', {
+mongoose.connect('mongodb://'+ process.env.MONGO_DB_USER + ':' + process.env.MONGO_DB_PW + '@webappproject-shard-00-00-b1buu.mongodb.net:27017,webappproject-shard-00-01-b1buu.mongodb.net:27017,webappproject-shard-00-02-b1buu.mongodb.net:27017/test?ssl=true&replicaSet=WebAppProject-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true
 });
 
